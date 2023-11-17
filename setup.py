547f7d051a390180e7e21b62d3f886a33bd6c8df
Kumari Rakhi <rakhij671@gmail.com>
2023-11-17 13:46:42 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="rakhi",
+    author_email="rakhij671@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
